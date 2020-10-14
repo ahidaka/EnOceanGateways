c3976a72008162b5575b422d38e523cf6e46c4a2
ahidaka <hidaka@devdrv.co.jp>
2020-10-14 12:51:03 +0000

    modify epclient

--- DolphinRide/epclient/app.js
+++ DolphinRide/epclient/app.js
@@ -3,14 +3,11 @@
 //var logger = require('./logger');
 //var utils = require('./utils');
 
-
 const SENSOR_PORT = 8000;
 const SENSOR_HOST = "127.0.0.1";
 const HTTP_PORT = 80;
 
 var net = require("net");
-var client = new net.Socket();
-
 var express = require('express');
 var app = express();
 
@@ -20,47 +17,117 @@ var app = express();
 app.use('/', express.static('public'));
 
 function main() {
+
+    //var client = new net.Socket();
+    const port = SENSOR_PORT;
+    const host = SENSOR_HOST;
+    const timeout = 5000;
+    let retrying = false;
+
     console.log('Starting...');
 
     var http_server = require('http').Server(app);
     var socketio = require('socket.io')(http_server);
 
     http_server.listen(HTTP_PORT, () => {
-    	console.log('listening on *:' + HTTP_PORT);
+        console.log('listening on *:' + HTTP_PORT);
     });
 
     socketio.on('connection', (socket) => {
-	console.log('sensor connected');
-	socket.on('disconnect', () => {
-	    console.log('sensor disconnected');
-    });
-    
-    client.connect(SENSOR_PORT, SENSOR_HOST, function() {
-    	console.log("connect to: " + SENSOR_HOST + ":" + SENSOR_PORT);
-    });
-
-    client.on("data", function(data) {
-    	//console.log(data.toString());
-	    //socketio.emit('message', data.toString());
-	    socketio.emit('message', function(d) {
-            var s = d.toString();
-            // Clear non printable character and omit error
-            s = s.replace(/\\n/g, "\\n")
-                .replace(/\\'/g, "\\'")
-                .replace(/\\"/g, '\\"')
-                .replace(/\\&/g, "\\&")
-                .replace(/\\r/g, "\\r")
-                .replace(/\\t/g, "\\t")
-                .replace(/\\b/g, "\\b")
-                .replace(/\\f/g, "\\f");
-            s = s.replace(/[\u0000-\u0019]+/g,"");
-            return s;
+        clearIntervalConnect();
+    	console.log('sensor connected');
+	    socket.on('disconnect', () => {
+	        console.log('sensor disconnected');
         });
     });
 
-    client.on("close", function() {
-	    console.log("disconnect from: " + SENSOR_HOST + ":" + SENSOR_PORT);
-    })
+    function memo() {
+    client.on("data", function(data) {
+    	//console.log(data.toString());
+        //socketio.emit('message', data.toString());
+        
+        var s = data.toString();
+        // Clear non printable character and omit error
+        s = s.replace(/\\n/g, "\\n")
+            .replace(/\\'/g, "\\'")
+            .replace(/\\"/g, '\\"')
+            .replace(/\\&/g, "\\&")
+            .replace(/\\r/g, "\\r")
+            .replace(/\\t/g, "\\t")
+            .replace(/\\b/g, "\\b")
+            .replace(/\\f/g, "\\f");
+        s = s.replace(/[\u0000-\u0019]+/g,"");
+        socketio.emit('message', s);
+    });
+    }
+
+    // Functions to handle socket events
+    function makeConnection () {
+        console.log('makeConnection');
+        client.connect(port, host);
+    }
+
+    function connectEventHandler() {
+        console.log('connected');
+        retrying = false;
+    }
+
+    function dataEventHandler(data) {
+        console.log('data');
+
+        var s = data.toString();
+        // Clear non printable character and omit error
+        s = s.replace(/\\n/g, "\\n")
+            .replace(/\\'/g, "\\'")
+            .replace(/\\"/g, '\\"')
+            .replace(/\\&/g, "\\&")
+            .replace(/\\r/g, "\\r")
+            .replace(/\\t/g, "\\t")
+            .replace(/\\b/g, "\\b")
+            .replace(/\\f/g, "\\f");
+        s = s.replace(/[\u0000-\u0019]+/g,"");
+        socketio.emit('message', s);
+    }
+
+    function endEventHandler() {
+        console.log('end');
+    }
+
+    function timeoutEventHandler() {
+        console.log('timeout');
+    }
+
+    function drainEventHandler() {
+        console.log('drain');
+    }
+
+    function errorEventHandler() {
+        console.log('error');
+    }
+
+    function closeEventHandler () {
+        console.log('close');
+        if (!retrying) {
+            retrying = true;
+            console.log('Reconnecting...');
+        }
+        setTimeout(makeConnection, timeout);
+    }
+
+    // Create socket and bind callbacks
+    let client = new net.Socket();
+    client.on('connect', connectEventHandler);
+    //client.on('data',    function(data) {dataEventHandler(data)});
+    client.on('data',    (data) => dataEventHandler(data));
+    client.on('end',     endEventHandler);
+    client.on('timeout', timeoutEventHandler);
+    client.on('drain',   drainEventHandler);
+    client.on('error',   errorEventHandler);
+    client.on('close',   closeEventHandler);
+
+    // Connect
+    console.log('Connecting to ' + host + ':' + port + '...');
+    makeConnection();
 
     //app.get('/', (req, res) => {
     //  res.sendFile(__dirname + 'index.html');
@@ -70,5 +137,6 @@ function main() {
 }
 
 if (require.main === module) {
-    main()
+    main();
+    console.log('main exit');
 }
